ca4daa63ec4b937efe58322743fe017a67213560
Aidarbek Kurmankozhoev <aidarbek.kurmankojoev.pls@gmail.com>
2022-12-05 13:04:55 +0600

    main is ready

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".MySwiper", {
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiperSlider = new Swiper(".mySwiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
@@ -15,15 +28,28 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-var swiper = new Swiper(".MySwiper", {
-  speed: 600,
-  parallax: true,
+const swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 3,
+  spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 });
+
+const acc = document.getElementsByClassName("faq__header");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
